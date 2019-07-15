ce195a2a67254974b2a723000a9abbb1c16efd01
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2019-07-15 22:07:59 +0900

    Add: Buefy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import '@/assets/style.scss'
 import router from './router'
 import store from './store'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
