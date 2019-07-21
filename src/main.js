63f9e2a7c2fab1cd71713d97a572382b0e71a983
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2019-07-21 15:05:26 +0900

    Add: VueAnalytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import VueHead from 'vue-head'
+import VueAnalytics from 'vue-analytics'
 import '@/assets/style.scss'
 import router from './router'
 import store from './store'
@@ -9,6 +10,10 @@ import store from './store'
 Vue.use(Buefy)
 Vue.use(VueHead, {
   separator: ' | '
+})
+Vue.use(VueAnalytics, {
+  id: 'UA-144246391-1',
+  router
 })
 Vue.config.productionTip = false
 
