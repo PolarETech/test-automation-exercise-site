4c57de1090c62e1beb09166e452d7002e92c2412
Polar Tech <52513329+PolarETech@users.noreply.github.com>
2019-07-24 15:14:49 +0900

    Add: e2e test for Login view

---
+++ tests/e2e/specs/login.js
@@ -0,0 +1,46 @@
+describe('Login view test', () => {
+  it('displays "Login" page', () => {
+    cy.visit('/login')
+    cy.title().should('eq', 'Login | test automation exercise site')
+    cy.get('header .navbar').should('be.visible')
+    cy.get('h1').should('contain', 'ログイン')
+    cy.get('#user-id-input').should('be.visible')
+    cy.get('#password-input').should('be.visible')
+    cy.get('button').should('contain', 'ログイン')
+    cy.get('#require-message').should('not.be.visible')
+    cy.get('.error-message').should('not.be.visible')
+    cy.contains('footer', '© 2019 Polar Tech').should('be.visible')
+  })
+
+  it('moves to TodoList view after selecting "Login" button with correct ID and Password', () => {
+    cy.visit('/login')
+    cy.get('input[type=text]').type('testID')
+    cy.get('input[type=password]').type('testPASS')
+    cy.contains('button', 'ログイン').click()
+    cy.title().should('eq', 'TodoList | test automation exercise site')
+    cy.getCookie('PtExampleToken').should('exist')
+      .then((cookieData) => {
+        const cookieValue = JSON.parse(decodeURI(cookieData.value))
+        expect(cookieValue.auth.token).to.eq('dummy-token')
+      })
+  })
+
+  it('shows error message after selecting "Login" button with wrong ID and Password', () => {
+    cy.visit('/login')
+    cy.get('input[type=text]').type('foo')
+    cy.get('input[type=password]').type('boo')
+    cy.contains('button', 'ログイン').click()
+    cy.title().should('eq', 'Login | test automation exercise site')
+    cy.get('.error-message')
+      .should('be.visible')
+      .and('contain', 'ログインエラーユーザーIDまたはパスワードが違います')
+  })
+
+  it('shows require log-in message if user accessed Login view by redirect', () => {
+    cy.visit('/todo')
+    cy.title().should('eq', 'Login | test automation exercise site')
+    cy.get('#require-message')
+      .should('be.visible')
+      .and('contain', 'ログインが必要です')
+  })
+})
